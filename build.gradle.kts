2b4c85108edb65cd25046ba12ad57fbe1a5932fa
Pratyush <codelab@pratyush.dev>
2022-11-24 01:16:04 +0530

    added gradle build support

---
+++ build.gradle.kts
@@ -0,0 +1,28 @@
+buildscript {
+    repositories {
+        // dependabot cannot handle google()
+        maven {
+            url = uri("https://dl.google.com/dl/android/maven2")
+        }
+        // dependabot cannot handle mavenCentral()
+        maven {
+            url = uri("https://repo.maven.apache.org/maven2")
+        }
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.20")
+    }
+}
+
+allprojects {
+    tasks.withType<JavaCompile> {
+        val compilerArgs = options.compilerArgs
+        compilerArgs.add("-Xlint:unchecked")
+        compilerArgs.add("-Xlint:deprecation")
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
